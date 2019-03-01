6581ac8b446825684396635eae289410b2118793
Bendjama Kassem <bendjama.kassem@gmail.com>
2019-03-01 12:22:34 +0100

    Ajoutez des fichiers projet.

---
+++ gsb/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace gsb
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.inputID = new System.Windows.Forms.TextBox();
+            this.inputMDP = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.submit = new System.Windows.Forms.Button();
+            this.errorConnection = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorConnection)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(284, 142);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nom";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(347, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Identification";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // inputID
+            // 
+            this.inputID.Location = new System.Drawing.Point(257, 162);
+            this.inputID.Name = "inputID";
+            this.inputID.Size = new System.Drawing.Size(100, 22);
+            this.inputID.TabIndex = 2;
+            this.inputID.TextChanged += new System.EventHandler(this.inputID_TextChanged);
+            // 
+            // inputMDP
+            // 
+            this.inputMDP.Location = new System.Drawing.Point(431, 162);
+            this.inputMDP.Name = "inputMDP";
+            this.inputMDP.Size = new System.Drawing.Size(100, 22);
+            this.inputMDP.TabIndex = 3;
+            this.inputMDP.TextChanged += new System.EventHandler(this.inputMDP_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(438, 142);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Mot de passe";
+            // 
+            // submit
+            // 
+            this.submit.Location = new System.Drawing.Point(350, 227);
+            this.submit.Name = "submit";
+            this.submit.Size = new System.Drawing.Size(75, 23);
+            this.submit.TabIndex = 5;
+            this.submit.Text = "Valider";
+            this.submit.UseVisualStyleBackColor = true;
+            this.submit.Click += new System.EventHandler(this.submit_Click);
+            // 
+            // errorConnection
+            // 
+            this.errorConnection.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.submit);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.inputMDP);
+            this.Controls.Add(this.inputID);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.errorConnection)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox inputID;
+        private System.Windows.Forms.TextBox inputMDP;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button submit;
+        private System.Windows.Forms.ErrorProvider errorConnection;
+    }
+}
+
---
+++ gsb/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gsb
+{
+    public partial class Form1 : Form
+    {
+        string chaineConnexion;
+        public string ChaineConnexion
+        {
+            get { return chaineConnexion; }
+            set { chaineConnexion = value; }
+        }
+
+        public Form1()
+        {
+             ChaineConnexion = "server=localhost;user=root;database=gsbcsharp";
+            InitializeComponent();
+
+        }
+       
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void inputID_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void inputMDP_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void submit_Click(object sender, EventArgs e)
+        {
+
+
+            CURS mdp = new CURS(ChaineConnexion);
+            string req = "SELECT COL_NOM, COL_DATEEMBAUCHE FROM collaborateur WHERE COL_NOM='" + inputID.Text+"';";
+            mdp.ReqSelect(req);
+            errorConnection.Clear();
+
+            string mdpdate = mdp.champ("COL_DATEEMBAUCHE").ToString();
+            mdp.fermer();
+
+            mdpdate = mdpdate[8, 9]+;
+            19-jan-2003
+                2003 - 06 - 18 00:00:00
+            if (true)
+            {
+                this.Hide();
+            }
+            else
+            {
+                errorConnection.SetError(submit, "Vérifier vote login ou votre mot de passe ! ");
+            }
+        }
+    }
+}
